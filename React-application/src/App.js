6b912b14cff2e45c58442473ddbe384fb7778ef0
verona <verona@veronas-MacBook-Air.local>
2023-07-31 13:23:03 -0400

    edited app.js

--- React-application/src/App.js
+++ React-application/src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hi there, this is Verona!</p>
+        <p>Hi wecloud devops, this is Verona!</p>
         <a
           className="App-link"
           href="https://reactjs.org"
